1414904b3f0b78c3fd7fc8efc28a5a945a0348ff
manish_kumar <manish.prasad@devdigital.com>
2024-03-11 17:57:51 +0530

    scanner

--- resources/views/backend/staff/attendance_mark.blade.php
+++ resources/views/backend/staff/attendance_mark.blade.php
@@ -1,21 +1,43 @@
-@extends('frontend.includes.main')
-@section('title', 'Mark Attendance')
-@section('style')
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
 
-@endsection
-@section('content')
-    <div class="page-content clear-medium ">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover">
+    <title>J.S.F. Academy | @yield('title')</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('frontend/assets/images/collage/JFS.png') }}" style="border-radius: 25%;">
+    <link rel="stylesheet" type="text/css" href="{{asset('frontend/assets/styles/bootstrap.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('frontend/assets/fonts/css/fontawesome-all.min.css')}}">
+    {{-- <link rel="manifest" href="_manifest.json" data-pwa-version="set_in_manifest_and_pwa_js"> --}}
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('frontend/assets/app/icons/icon-192x192.png')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
 
-        <div class="card card-style ">
-    <h1 class="text-center">QR Scanner</h1>
-
-            <div id="qr-reader" ></div>
-            <div id="qr-reader-results"></div>
-        </div>
+<body class="theme-light" data-highlight="highlight-red" data-gradient="body-default">
+    <div id="preloader">
+        <div class="spinner-border color-highlight" role="status"></div>
     </div>
-@endsection
-@section('script')
+    <div id="page">
+
+        @include('frontend.includes.header')
+
+        <div class="page-content header-clear-small">
+            <div id="qr-reader" style="width:450px" ></div>
+            <div id="qr-reader-results"></div>
+
+
+        </div>
+
+        @include('frontend.includes.footer')
+
+        @include('frontend.includes.foot')
+
+
+    </div>
     <script src="https://unpkg.com/html5-qrcode"></script>
+
     <script>
         var resultContainer = document.getElementById('qr-reader-results');
         var lastResult, countResults = 0;
@@ -37,4 +59,6 @@
         html5QrcodeScanner.render(onScanSuccess);
     </script>
 
-@endsection
+</body>
+
+</html>
--- resources/views/frontend/includes/main.blade.php
+++ resources/views/frontend/includes/main.blade.php
@@ -11,29 +11,31 @@
 <!-- BEGIN: Body-->
 
 <body class="theme-light" data-highlight="highlight-red" data-gradient="body-default">
-    <div id="preloader"><div class="spinner-border color-highlight" role="status"></div></div>
+    <div id="preloader">
+        <div class="spinner-border color-highlight" role="status"></div>
+    </div>
     <div id="page">
 
-    <!-- BEGIN: Header-->
-    @include('frontend.includes.header')
-    <!-- END: Header-->
+        <!-- BEGIN: Header-->
+        @include('frontend.includes.header')
+        <!-- END: Header-->
 
-    <!-- BEGIN: Content-->
-    <div class="page-content header-clear-small">
+        <!-- BEGIN: Content-->
+        <div class="page-content header-clear-small">
             {{-- @include('layout.breadcrumb') --}}
-                @yield('content')
-            </div>
+            @yield('content')
+        </div>
 
-    <!-- END: Content-->
-    <!-- BEGIN: Footer-->
-    @include('frontend.includes.footer')
-    <!-- END: Footer-->
+        <!-- END: Content-->
+        <!-- BEGIN: Footer-->
+        @include('frontend.includes.footer')
+        <!-- END: Footer-->
 
-    <!-- BEGIN: script-->
-    @include('frontend.includes.foot')
+        <!-- BEGIN: script-->
+        @include('frontend.includes.foot')
 
-    <!-- END: script-->
-    @yield('script')
+        <!-- END: script-->
+        @yield('script')
     </div>
 </body>
 <!-- END: Body-->
